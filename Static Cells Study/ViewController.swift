0a2fd8887eb52d0a794c51c344d9177342d5a074
Sam Lee <sam1990790120@gmail.com>
2020-09-07 16:59:19 +0800

    Initial Commit

---
+++ Static Cells Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Static Cells Study
+//
+//  Created by 李世文 on 2020/9/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

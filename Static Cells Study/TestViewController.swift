1b1f2c9a6fa953a8aef5ddf11e5e1b0c7d21a5b9
Sam Lee <sam1990790120@gmail.com>
2020-09-09 17:14:55 +0800

    練習 static 表格

---
+++ Static Cells Study/TestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestViewController.swift
+//  Static Cells Study
+//
+//  Created by 李世文 on 2020/9/8.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
